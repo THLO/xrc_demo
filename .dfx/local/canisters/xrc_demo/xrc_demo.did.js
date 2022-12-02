713a643616c04c5b16e0811ff3e51d7414e8e983
Thomas Locher <thomas.locher@dfinity.org>
2022-12-02 15:44:37 +0100

    Updated the XRC wasm file.

--- .dfx/local/canisters/xrc_demo/xrc_demo.did.js
+++
@@ -1,6 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'get_exchange_rate' : IDL.Func([IDL.Text], [IDL.Float64], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
